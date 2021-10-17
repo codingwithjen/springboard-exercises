fb8388c04f57f4e14884fdd3c1754941988774c6
jen f <77820429+jenfausto@users.noreply.github.com>
2021-10-16 22:23:42 -0600

    fixes

--- js-todos/todo.js
+++ js-todos/todo.js
@@ -1,51 +1,28 @@
-const todoForm = document.getElementById("todoForm");
-const todoList = document.getElementById("todoList"); // Where your newLi will appear
+document.addEventListener("DOMContentLoaded", function() {
+    let todoForm = document.getElementById("todoForm");
+    let todoList = document.getElementById("todoList"); // Where your newLi will appear
 
-// Retrieve from localStorage
-const submittedTodos = JSON.parse(localStorage.getItem("todos")) || [];
-for (let i = 0; i < submittedTodos.length; i++) {
-    let newTodo = document.createElement("li");
-    newTodo.innerText = submittedTodos[i].task;
-    newTodo.isDone = submittedTodos[i].isDone ? true : false;
-    if (newTodo.isDone) {
-        newTodo.style.textDecoration = "line-through";
-    }
-    todoList.appendChild(newTodo);
-}
+    todoForm.addEventListener("submit", function(event) {
+      event.preventDefault(); // Stops the page from refreshing entirely
 
-todoForm.addEventListener("submit", function(event) {
-    event.preventDefault();
+      let removeBtn = document.createElement("button");
+      removeBtn.innerText = "X";
 
-    let newTodo = document.createElement("li");
-    let submittedTodo = document.getElementById("task").value;
-    newTodo.innerText = submittedTodo;
-    newTodo.isDone = false;
-    todoList.appendChild(newTodo);
-    todoForm.reset();
+      let newTodo = document.createElement("li");
+      newTodo.innerText = document.getElementById("task").value; // Whatever value was entered/submitted in the input
 
-    // Save to localStorage
-    submittedTodos.push({ task: newTodo.innerText, isDone: false });
-    localStorage.setItem("todos", JSON.stringify(submittedTodos));
-});
+      todoList.appendChild(newTodo); // <li> is a child because of the <ul> in the HTML
+      newTodo.appendChild(removeButton);
 
-todoList.addEventListener("click", function(event) {
-    const item = event.target.tagName;
-    let clickedItem = event.target;
+      todoForm.reset(); // clear all the values of the form elements - sets values to default
+    });
 
-    if (!clickedItem.isDone) {
-        clickedItem.style.textDecoration = "line-through";
-        clickedItem.isDone = true;
-    } else {
-        clickedItem.style.textDecoration = "none";
-        clickedItem.isDone = false;
-    }
-
-    for (let i = 0; i < submittedTodos.length; i++) {
-        if (submittedTodos[i].task === clickedItem.innerText) {
-            submittedTodos[i].isDone = !submittedTodos.isDone;
-            localStorage.setItem("todos", JSON.stringify(submittedTodos));
-        }
-    }
-});
-
-
+    todoList.addEventListener("click", function(event) {
+      const item = event.target.tagName;
+      if (item === "LI") {
+        event.target.style.textDecoration = "line-through";
+      } else if (item === "BUTTON") {
+        event.target.parentNode.remove();
+      }
+    });
+  });
